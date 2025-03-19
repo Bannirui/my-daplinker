7ad74b39afa08de8ce1ab9cee73ed781c00f1041
dingrui <Bannirui@outlook.com>
2025-03-19 23:22:50 +0800

    openocd烧录脚本

--- software/my_demo/flash.sh
+++ software/my_demo/flash.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 openocd \
--s /usr/share/openocd/scripts \
+-s /usr/local/Cellar/open-ocd/0.12.0_1/share/openocd/scripts \
 -f openocd.cfg \
 -c "program build/my_demo.elf" \
 -c "sleep 200" \
