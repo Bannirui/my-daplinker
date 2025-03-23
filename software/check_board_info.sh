64ad0d05669033bf149c59c94703687cb9dc27d9
dingrui <Bannirui@outlook.com>
2025-03-23 23:13:15 +0800

    编译

--- software/check_board_info.sh
+++ software/check_board_info.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-
+# 查看板子flash布局
 openocd \
--c "set CPUTAPID 0" \
+-c "set CPUTAPID 0x2ba01477" \
 -f /usr/local/Cellar/open-ocd/0.12.0_1/share/openocd/scripts/interface/stlink.cfg \
 -c "transport select hla_swd" \
 -f /usr/local/Cellar/open-ocd/0.12.0_1/share/openocd/scripts/target/stm32f1x.cfg \
--c "program build/my_demo.bin" \
--c "sleep 200" \
--c "reset run" \
--c exit
+-c "init" \
+-c "reset halt" \
+-c "flash info 0"
--- software/flash_bootloader.sh
+++ software/flash_bootloader.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 # 烧录DAPLink的bootloader
-
 openocd \
--c "set CPUTAPID 0" \
+-c "set CPUTAPID 0x2ba01477" \
 -f /usr/local/Cellar/open-ocd/0.12.0_1/share/openocd/scripts/interface/stlink.cfg \
 -c "transport select hla_swd" \
 -f /usr/local/Cellar/open-ocd/0.12.0_1/share/openocd/scripts/target/stm32f1x.cfg \
--c "program ./DAPLink/build/stm32f103xb_bl.elf" \
--c "sleep 200" \
+-c "init" \
+-c "reset halt" \
+-c "stm32f1x unlock 0" \
+-c "reset halt" \
+-c "flash erase_address 0x08002000 0x2000" \
+-c "flash write_image erase ./DAPLink/build/stm32f103xb_bl.elf 0x08002000" \
+-c "verify_image ./DAPLink/build/stm32f103xb_bl.elf 0x08002000" \
 -c "reset run" \
--c exit
+-c "shutdown"
