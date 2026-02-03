80be8de218d95c5f6c2898457e4f9c627e31b16f
Bannirui <Bannirui@outlook.com>
2026-02-03 23:15:54 +0800

    upload stm32f103c8t6

---
+++ software/STM32F103C8T6/flash_bl.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+TARGET_NAME="STM32F103C8T6.elf"
+TARGET_PATH="$SCRIPT_DIR/build/Debug/$TARGET_NAME"
+
+if [ ! -f "$TARGET_PATH" ]; then
+    echo "ERROR: $TARGET_NAME ELF not found:"
+    exit 1
+fi
+
+echo "the target is: $TARGET_NAME"
+
+openocd \
+  -s /usr/share/openocd/scripts \
+  -f openocd.cfg \
+  -c "program $TARGET_PATH verify reset exit"
